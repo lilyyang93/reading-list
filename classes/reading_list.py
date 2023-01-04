019ec9e7d071da2fc72f0d81f5fadcbb8cc564a0
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2023-01-04 09:33:24 -1000

    addressed interface bug in add_to_reading_list method

--- classes/reading_list.py
+++ classes/reading_list.py
@@ -1,18 +1,23 @@
+#functionality related to getting or updating a user's reading list belong in this class
+
 reading_list = []
 
 class ReadingList:
-    def add_to_reading_list(response, items_retrieved):
+    def add_to_reading_list(books):
         selection_2 = input("\nSelect a book number to add it to your reading list. Press 0 to return home.\n\n>>>")
+
         try: 
-            if int(selection_2) >= 1 and int(selection_2) <= items_retrieved:
-                my_book = int(selection_2)
-                reading_list.append(response[my_book-1]["volumeInfo"]["title"])
+            if int(selection_2) >= 1 and int(selection_2) <= len(books):
+                book_num = int(selection_2)-1
+                reading_list.append(books[book_num].title)
                 print("\nYour reading list was updated.")
 
             elif int(selection_2) == 0:
                 print("\nReturning home.")
+            elif int(selection_2) > len(books):
+                print("\nInvalid option, please try again.")
         except:
-            print("\nPlease enter a valid option")
+            print("\nInvalid option, please try again.")
 
     def get_reading_list():
         if (len(reading_list) == 0):
