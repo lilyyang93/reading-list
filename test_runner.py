efc342669628a70d29fc9700cc1dfb15d38584c3
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2023-01-04 10:26:16 -1000

    created test suite for runner, added test goals only

---
+++ test_runner.py
@@ -0,0 +1,13 @@
+import unittest
+from . import runner 
+
+"""
+
+app should handle:
+-correctly move through program with correct inputs from user
+-alert user of invalid inputs without program crashing 
+
+"""
+
+if __name__ == '__main__':
+    unittest.main()
